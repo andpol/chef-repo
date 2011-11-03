9821fe81e36df7e8af9351c164d67af5f94b0651
Andrey <andpol28325@gmail.com>
2011-11-03 15:41:42 -0400

    adding two roles

---
+++ roles/node1.rb
@@ -0,0 +1,11 @@
+###################
+name "node1"
+description "This is node 1"
+run_list "recipe[ipchange]"
+
+#Default attributes only get applied if they do not exist already on node
+default_attributes 
+
+#Override attributes overwrites existing values
+override_attributes "ifconfig" => {"ip_address" => "192.168.1.1"}
+###################
